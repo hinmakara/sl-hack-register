747ec381cd84504c1f774c3743d6acfcb7a5fbab
Makara HIN <hinmakara@gmail.com>
2021-02-26 07:32:17 +0000

    testing

--- securimage_solver/test.py
+++ securimage_solver/test.py
@@ -1,80 +1,37 @@
 import captcha_api
-import urllib.request
+import requests
 import os.path
-import http.client
-import mimetypes
-from codecs import encode
+
 
 def main():
 
-    conn = http.client.HTTPSConnection("swordslegends.com")
-    dataList = []
-    boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
-    dataList.append(encode('--' + boundary))
-    dataList.append(encode('Content-Disposition: form-data; name=username;'))
+    cookie = 'PHPSESSID=72d03bd5a5b8f37fc9c87ac1b2f3ca75'
+    username = 'mrapplehack1234'
+    email = 'mrapplehack1234@test.com'
+    password = 'mrapplehack1234'
+    password_again = 'mrapplehack1234'
+    captcha_code = ''
+    register = 'Register'
 
-    dataList.append(encode('Content-Type: {}'.format('text/plain')))
-    dataList.append(encode(''))
+    response = requests.request(
+        "POST",
+        "https://swordslegends.com/swords/register.html",
+         headers = {
+             'cookie': cookie
+         },
+         data = {
+            'username': username,
+            'email': email,
+            'password': password,
+            'password_again': password_again,
+            'captcha_code': captcha_code,
+            'register': register
+         },
+         files = [
+         ])
 
-    dataList.append(encode("hacker173"))
-    dataList.append(encode('--' + boundary))
-    dataList.append(encode('Content-Disposition: form-data; name=email;'))
-
-    dataList.append(encode('Content-Type: {}'.format('text/plain')))
-    dataList.append(encode(''))
-
-    dataList.append(encode("hacker173@test.com"))
-    dataList.append(encode('--' + boundary))
-    dataList.append(encode('Content-Disposition: form-data; name=password;'))
-
-    dataList.append(encode('Content-Type: {}'.format('text/plain')))
-    dataList.append(encode(''))
-
-    dataList.append(encode("hacker173"))
-    dataList.append(encode('--' + boundary))
-    dataList.append(encode('Content-Disposition: form-data; name=password_again;'))
-
-    dataList.append(encode('Content-Type: {}'.format('text/plain')))
-    dataList.append(encode(''))
-
-    dataList.append(encode("hacker173"))
-    dataList.append(encode('--' + boundary))
-    dataList.append(encode('Content-Disposition: form-data; name=captcha_code;'))
-
-    dataList.append(encode('Content-Type: {}'.format('text/plain')))
-    dataList.append(encode(''))
-
-    conn.request("POST","https://swordslegends.com/swords/libs/securimage/securimage_show.html")
-    res = conn.getresponse()
-    data = res.read()
-    file = open("tmp.png", "wb")
-    file.write(data)
-    file.close()
-    captcha_code = captcha_api.predict('tmp.png')
-
-    dataList.append(encode(captcha_code))
-    dataList.append(encode('--' + boundary))
-    dataList.append(encode('Content-Disposition: form-data; name=register;'))
-
-    dataList.append(encode('Content-Type: {}'.format('text/plain')))
-    dataList.append(encode(''))
-
-    dataList.append(encode("Register"))
-    dataList.append(encode('--'+boundary+'--'))
-    dataList.append(encode(''))
-    body = b'\r\n'.join(dataList)
-    payload = body
-    headers = {
-        'Cookie': 'PHPSESSID=8528f6269c7c459d5cf9c837010ed440',
-        'Content-type': 'multipart/form-data; boundary={}'.format(boundary)
-    }
-
-    conn.request("POST", "/swords/register.html", payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
-
-    print(captcha_code)
+    print(response.text)
+    # print(captcha_code)
 
 if __name__ == '__main__':
     main()
